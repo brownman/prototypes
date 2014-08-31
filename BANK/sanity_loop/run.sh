47001cde262f21807f977bf03e4046cb4a420058
brownman <brownman556@gmail.com>
2014-08-31 14:27:15 +0300

    [ update the prototype library ] - need to make it independant by supplying a submodule address

--- BANK/sanity_loop/run.sh
+++ BANK/sanity_loop/run.sh
@@ -1,10 +1,45 @@
-set -e
+clear
+#set -e
+set -u
+source /tmp/library_proto.cfg
+pushd `dirname $0` >/dev/null
+
 loop(){
   local delay=${1:-60}
   while :;do
-    commander     $builtin_commitment $delay
+commander     $builtin_commitment $delay
+breaking
   done
 }
+set_env(){
+  local file_list=./DEPLOY/INSTALL/required.txt
+  cat  $file_list 
+  local cmd
+  while read line;do
+    cmd="use $line"
+    echo "[CMD] $cmd"
+#commander_fallback    "$cmd" "exit 1"
+commander $cmd
+  done < $file_list
+}
+testing(){
+return 0
+}
 
-loop ${@:-}
+validate_file_lang(){
+  export file_language=.config/langs
 
+  if [ ! -f $file_language ];then
+    touch $file_language
+    echo -e "ru\nit\nar\nhi" > $file_language
+  fi
+}
+
+
+
+
+use commander
+set_env
+validate_file_lang
+testing && loop ${@:-}
+popd >/dev/null
---
+++ TOOL/generate_link.sh
@@ -0,0 +1,32 @@
+set -u
+clear
+str1="$1"
+str2="${2:-cfg}"
+
+
+#dir_root=$PWD
+#echo dir_root: $dir_root
+
+test $# -gt 0 || { echo supply a funcname ; exit; }
+source /tmp/library.cfg
+use print
+use assert
+use indicator
+use commander
+
+
+
+file=$( commander finder "$str1" "$str2"  )
+res=$?
+if [ $res -eq 0  ];then
+  #if [ -n "$file" ];then
+  commander assert file_exist $file
+
+
+  filename=`basename $file`
+  dir_target='./COMMON/BANK'
+  commander assert dir_exist $dir_target
+  file_target="$dir_target/$filename"
+  [ -L $file_target ] && ( print color 33  file already exist: $file_target ) || (  commander ln -s $file $file_target ;   commander
+  assert symlink_exist $file_target )
+fi
