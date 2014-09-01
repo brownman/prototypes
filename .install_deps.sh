e10328b0030c851fa895e092a24618a2b381b98a
brownman <brownman556@gmail.com>
2014-09-01 10:57:36 +0300

    install without confirmation

--- .install_deps.sh
+++ .install_deps.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -y fortune pv toilet curl wget tree gxmessage yad zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
-sudo add-apt-repository ppa:webupd8team/y-ppa-manager 
+yes | sudo add-apt-repository ppa:webupd8team/y-ppa-manager 
 sudo apt-get update 
 sudo apt-get install yad
 
