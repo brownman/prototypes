80cfe3522011be709a2c37e8cb64a2818d31c048
brownman <brownman556@gmail.com>
2014-09-01 11:05:10 +0300

    [travis] use tool:yes for simulate pressing the y\n key

--- .install_deps.sh
+++ .install_deps.sh
@@ -1,5 +1,8 @@
-sudo apt-get install -y fortune pv toilet curl wget tree gxmessage yad zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
-yes | sudo add-apt-repository ppa:webupd8team/y-ppa-manager 
+install1(){
+  sudo apt-get install -y fortune pv toilet curl wget tree gxmessage yad zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
+sudo add-apt-repository ppa:webupd8team/y-ppa-manager 
 sudo apt-get update 
 sudo apt-get install yad
+}
+yes | install1
 
