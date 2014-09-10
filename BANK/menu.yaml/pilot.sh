b6c8328d85f7b322d816dbb9f343f0739b293cfe
brownman <brownman556@gmail.com>
2014-09-10 15:03:41 +0300

    [ menu.yaml ] - updated menu.yaml

--- BANK/menu.yaml/pilot.sh
+++ BANK/menu.yaml/pilot.sh
@@ -20,208 +20,212 @@ exec 2> >( tee /tmp/err )
 
 pushd `dirname $0` >/dev/null
 source_pyenv(){
- # set +u
- set +o nounset
- set -o | grep nounset
-#exiting 
+  # set +u
+  set +o nounset
+  set -o | grep nounset
+  #exiting 
 
-    #test -f $file_python
-    #assert file_exist "$file_python"
-#    ls -l $file_python
-#  commander  source $file_python
+  #test -f $file_python
+  #assert file_exist "$file_python"
+  #    ls -l $file_python
+  #  commander  source $file_python
 
   #set -u
-#  commander command shyaml && ( print ok we have shyaml loaded ) || { print error;  exiting ; }
-  
+  #  commander command shyaml && ( print ok we have shyaml loaded ) || { print error;  exiting ; }
+
 }
 
 ensure_anchor(){
-    file_self=$(who_am_i $0 )
-    if [ ! /tmp/pilot.sh -ef "$file_self" ];then
-      rm1 /tmp/pilot.sh
+  file_self=$(who_am_i $0 )
+  if [ ! /tmp/pilot.sh -ef "$file_self" ];then
+    rm1 /tmp/pilot.sh
     commander ln -s $file_self /tmp 
-    fi
+  fi
 }
 
 using(){
   use ps1
   use ps4
-    use where_am_i
-    use dialog_confirm
-    use rm1
-    use exiting
-    use dialog_menu
-    use commander
-    use print
-    use assert
-    use cat1
-    use dialog_menu
-    use who_am_i
-    use vars
+  use where_am_i
+  use dialog_confirm
+  use rm1
+  use exiting
+  use commander
+  use print
+  use assert
+  use cat1
+  use dialog_menu
+  use who_am_i
+  use vars
 
 }
 broadcast1(){
-    echo xcowsay "$@"
+  echo xcowsay "$@"
 }
 
 act_on_list1 () 
 { 
-    local file=$1;
-    local cmd=${2:-source};
-    while read line; do
-        local         cmd1="$cmd $line";
-        echo "$cmd1";
-        eval "$cmd1"
-    done < $file
+  local file=$1;
+  local cmd=${2:-source};
+  while read line; do
+    local         cmd1="$cmd $line";
+    echo "$cmd1";
+    eval "$cmd1"
+  done < $file
 }
 
 test_type(){
-    str="$1"
-    #trap trap_err ERR
-    #  text="${(type $str):-$(echo $str)}"
-    text=$(type $str)
-    ( gxmessage "$text" )
+  str="$1"
+  #trap trap_err ERR
+  #  text="${(type $str):-$(echo $str)}"
+  text=$(type $str)
+  ( gxmessage "$text" )
 }
 
 
 menu_subject(){
 
-    set -o nounset
-    #set -e
-    #trap trap_err ERR
-    local menu_name="$1"
-    commander assert string_has_content "$menu_name"
-    #print ok $FUNCNAME 
-    local cmd="parse_subject_menus '$menu_name'"
-    commander  "$cmd"
+  set -o nounset
+  #set -e
+  #trap trap_err ERR
 
-    local  str=$( dialog_menu_multiple /tmp/subject menu.yaml )
-    #log1 "$str"
-    [ -z "$str" ]  && { print error 'got empty string';exit 0; }
 
-    match_name "$str"
-    local res=$?
-    #indicator "$res"
-    if [ $res -eq 1 ];then
-        broadcast1 "sub-menu" &
-        eval "menu_subject \"$str\""
+  local menu_name="$1"
+  local str1 str cmd res
+
+  commander assert string_has_content "$menu_name"
+  #print ok $FUNCNAME 
+  cmd="parse_subject_menus '$menu_name'"
+  commander  "$cmd"
+
+  str=$( dialog_menu_multiple /tmp/subject menu.yaml )
+
+  #log1 "$str"
+  [ -z "$str" ]  && { print error 'got empty string';exit 0; }
+
+  match_name "$str"
+  res=$?
+  #indicator "$res"
+  if [ $res -eq 1 ];then
+    broadcast1 "sub-menu" &
+    eval "menu_subject \"$str\""
+  else
+
+    broadcast1 "command" &
+print color 33 "[ACTION] $str"
+#    str1="$( echo $str )"
+    if [ "$MODE_CONFIRM" = true ];then
+      dialog_confirm "$str" &
     else
-
-        broadcast1 "command" &
-        echo "[ACTION] $str"
-        str1="$( echo $str )"
-        if [ $MODE_CONFIRM = true ];then
-            dialog_confirm "$str1"
-        else
-            echo "$str"
-            local cmd_final=$( eval echo "$str" )
-            commander "$cmd_final"
-        fi
+ #     echo "$str"
+#      local cmd_final=$( "$str" )
+eval "$str" &
     fi
+  fi
 }
 source_lib(){
-    #trap trap_err ERR
-    MODE=0
-    shopt -s expand_aliases
-    }
+  #trap trap_err ERR
+  MODE=0
+  shopt -s expand_aliases
+}
 
 trap_err(){
-    broadcast1 trap err
-    local str="$( caller )"
-    #gxmessage "$str"
+  broadcast1 trap err
+  local str="$( caller )"
+  #gxmessage "$str"
 
-    local cmd="gvim +${str}"
-    echo "$cmd"
-    if [ -s /tmp/err ];then
-        cat /tmp/err
-        cmd=$( gxmessage -file /tmp/err -title trap_err -entrytext "$cmd" )
-        echo $cmd
-        eval "$cmd"
-    fi
-    exit 0
+  local cmd="gvim +${str}"
+  echo "$cmd"
+  if [ -s /tmp/err ];then
+    cat /tmp/err
+    cmd=$( gxmessage -file /tmp/err -title trap_err -entrytext "$cmd" )
+    echo $cmd
+    eval "$cmd"
+  fi
+  exit 0
 }
 
 
 parse_parent_menus(){
-    cat $file_menu | shyaml keys > /tmp/parents
+  cat $file_menu | shyaml keys > /tmp/parents
 }
 parse_subject_menus(){
-    cat $file_menu | shyaml get-value $menu_name > /tmp/subject
+  cat $file_menu | shyaml get-value $menu_name > /tmp/subject
 }
 parse_record(){
-    local str="$@"
-    local cmd=$( cat $file_menu | shyaml get-value "$str" )
-    echo "$cmd"
-    #> /tmp/subject
+  local str="$@"
+  local cmd=$( cat $file_menu | shyaml get-value "$str" )
+  echo "$cmd"
+  #> /tmp/subject
 }
 
 update_env(){
-    exec 2> >( tee -a /tmp/err )
-    exec 1> >( tee -a /tmp/out_long )
-    echo
+  exec 2> >( tee -a /tmp/err )
+  exec 1> >( tee -a /tmp/out_long )
+  echo
 }
 set_env(){
 
-    dir_self=`where_am_i $0`
-    file_menu=$dir_self/menu.yaml
-    dir_python=$dir_self/PYTHON_ENV/MAKE_ENV/env/bin/
-#    file_python=$dir_self/PYTHON_ENV/MAKE_ENV/env/bin/activate
+  dir_self=`where_am_i $0`
+  file_menu=$dir_self/menu.yaml
+  dir_python=$dir_self/PYTHON_ENV/MAKE_ENV/env/bin/
+  #    file_python=$dir_self/PYTHON_ENV/MAKE_ENV/env/bin/activate
 
-    #file_python=$dir_python/python2.7
-    #file_shyaml="$dir_python/shyaml"
-export    PATH=$PATH:$dir_python
+  #file_python=$dir_python/python2.7
+  #file_shyaml="$dir_python/shyaml"
+  export    PATH=$PATH:$dir_python
 
-    source $dir_self/match_name.cfg
+  source $dir_self/match_name.cfg
 }
 validate_list(){
-    assert file_exist $file_menu
-    #commander cat1 $file_menu true
-    echo cat1 $file_menu true
+  assert file_exist $file_menu
+  #commander cat1 $file_menu true
+  echo cat1 $file_menu true
 }
 intro(){
-    xcowsay pilot &
-    flite -t pilot &
+  xcowsay pilot &
+  flite -t pilot &
 
-    broadcast1 "$time" &
-    every 10 "broadcast1 \"$date_ws\"" &
+  broadcast1 "$time" &
+  every 10 "broadcast1 \"$date_ws\"" &
 }
 validate_no_errors(){
-    print_color 33 $FUNCNAME
-    ( gxmessage1 /tmp/err ) 
-    ( gxmessage1 /tmp/out_long )
+  print_color 33 $FUNCNAME
+  ( gxmessage1 /tmp/err ) 
+  ( gxmessage1 /tmp/out_long )
 }
 
 steps(){
-    #rm1 /tmp/err
-    #rm1 /tmp/out_long
-    #source libs
+  #rm1 /tmp/err
+  #rm1 /tmp/out_long
+  #source libs
 
 
 
-    source /tmp/library.cfg
+  source /tmp/library.cfg
 
-    using
-    set_env
-
-
-
-    ensure_anchor
-    validate_list
+  using
+  set_env
 
 
-    source_pyenv
-    #intro
-    #detect: user is cron/hotkey/shell
-    menu_subject main
+
+  ensure_anchor
+  validate_list
+
+
+  source_pyenv
+  #intro
+  #detect: user is cron/hotkey/shell
+  menu_subject main
 
 }
 
 MODE_CONFIRM=false
 if [ $# -eq 0 ];then
-    steps
+  steps
 else
-    echo set_env
-    echo  parse_record "$@"
+  echo set_env
+  echo  parse_record "$@"
 fi
 
 cat1 /tmp/err true
---
+++ README.md
@@ -0,0 +1,7 @@
+PROTOTYPES:
+===
+
+- run the library installer: LIB/install.sh
+- check out a prototype by running its: ./run.sh 
+
+[![Build Status](https://travis-ci.org/brownman/prototypes.svg?branch=develop)](https://travis-ci.org/brownman/prototypes)
