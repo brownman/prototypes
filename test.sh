0bf888d4c001c105f9a96ee5f818f109d369f80e
brownman <brownman556@gmail.com>
2014-09-15 15:06:37 +0300

    [ travis + x11 + audio ] - record screencast then post the file using curl

---
+++ test.sh
@@ -0,0 +1,7 @@
+./BANK/sanity_loop/run.sh
+
+  export AUDIODEV=null #travis - fix: no audio device 
+
+  export AUDIODEV=null #travis - fix: no audio device 
+    - "export DISPLAY=:99.0"
+      - ÃŸh -e /etc/init.d/xvfb start"
